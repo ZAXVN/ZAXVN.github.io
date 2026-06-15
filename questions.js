873f6e09b6c4c483965b2e1b5215a970f1f694f7
ZAXVN <akintanisrael3775@icloud.com>
2026-06-15 12:14:58 +0100

    Add questions database

---
+++ questions.js
@@ -0,0 +1,27 @@
+const questions = {
+  math: [
+    {
+      q: "What is 2 + 2?",
+      options: ["1", "2", "3", "4"],
+      answer: "4"
+    },
+    {
+      q: "What is 5 × 3?",
+      options: ["15", "10", "20", "25"],
+      answer: "15"
+    }
+  ],
+
+  english: [
+    {
+      q: "Choose the correct spelling:",
+      options: ["Recieve", "Receive", "Receeve", "Recive"],
+      answer: "Receive"
+    },
+    {
+      q: "Opposite of 'hot'?",
+      options: ["Cold", "Warm", "Heat", "Fire"],
+      answer: "Cold"
+    }
+  ]
+};
